30a7eaafcdea28d1dd2072452fdc09d8da31a0ab
Svenja Lebenhagen <svenja.lebenhagen@hpe.com>
2019-05-26 21:20:50 +0200

    Öffnen eines Bildes
    Zum Testen für Farb- und Formerkennung

--- CV.py
+++ CV.py
@@ -1,12 +1,18 @@
 import cv2
 
 #connection to webcam
-cam= cv2.VideoCapture(0)
-while(True):
-    ret, video = cam.read()
-    video = cv2.flip(video,1)
-    cv2.imshow('Video', video)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-cam.release()
+#cam= cv2.VideoCapture(0)
+#while(True):
+#    ret, video = cam.read()
+#    video = cv2.flip(video,1)
+#    cv2.imshow('Video', video)
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#        break
+#cam.release()
+#cv2.destroyAllWindows()
+
+#load image
+img = cv2.imread('Testbild.jpg', 1)
+cv2.imshow('Image', img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
