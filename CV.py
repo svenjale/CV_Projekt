0a95eb1f2928274e0824b79dfccbf936ace0b98a
Svenja Lebenhagen <svenja.lebenhagen@hpe.com>
2019-06-06 19:37:19 +0200

    Working Version: Gewinnererkennung und -markierung

--- CV.py
+++ CV.py
@@ -13,15 +13,16 @@ def main():
     winnery = False
 
     # load image and convert to gray
-    img = cv.imread('image.jpg', 1)
+    img = cv.imread('Test.jpg', 1)
+    img = cv.resize(img, (0, 0), fx=0.5, fy=0.5)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     gray = cv.medianBlur(gray, 5)
 
     # detect circles
     rows = gray.shape[0]
     circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
-                              param1=100, param2=30,
-                              minRadius=5, maxRadius=60)
+                              param1=100, param2=20,
+                              minRadius=1, maxRadius=40)
 
     # draw circles and center
     if circles is not None:
@@ -38,7 +39,7 @@ def main():
             # printing and counting red
             if 80 > color[0] >= 0 and 80 > color[1] >= 0 and 255 >= color[2] > 130:
                 farbe = "Rot"
-                zr = zr+1
+                zr = zr + 1
             else:
                 # printing and counting yellow
                 if 180 > color[0] >= 0 and 235 >= color[1] > 130 and 255 > color[2] > 125:
@@ -61,8 +62,9 @@ def main():
 
         # detect game situation - is there a winner?
         if winnery == False:
-            winnery = vertwinner(test)
-            winnery = horwinner(test)
+            winnery = vertwinner(test, img, radius)
+        if winnery == False:
+            winnery = horwinner(test, img, radius)
         # diagonal winner
         # ???
 
@@ -70,8 +72,8 @@ def main():
         if winnery == False:
             next(start, zr, zg)
 
-        # where is a free spot
-        lastfree(test, img, radius)
+            # where is a free spot
+            lastfree(test, img, radius)
 
     # show image and detected circles
     cv.imshow("Vier Gewinnt", img)
@@ -90,71 +92,100 @@ def sortboard (my_variable_array):
 
 
 # vertical winner function
-def vertwinner(test):
+def vertwinner(test, img, radius):
     rot = 0
     gelb = 0
     winnery = False
+    centerlist = []
     for x in range(0, 7):
         spalte = test[x]
 
         for myvariable in spalte:
             if myvariable[2] == "Rot":
                 rot = rot + 1
+                centerlist.append(myvariable)
                 if rot == 4:
-                    print("Rot hat in Spalte", x+1, "gewonnen!")
+                    print("Rot hat in Spalte", x+1, "von Links gewonnen!")
                     winnery = True
+                    # mark winning pieces
+                    for myvariable in centerlist:
+                        center = (myvariable[0], myvariable[1])
+                        #cv.circle(img, center, 1, (255, 255, 255), 2)
+                        cv.circle(img, center, radius, (255, 255, 255), 3)
                     return winnery
             else:
                 rot = 0
+                centerlist = []
 
         for myvariable in spalte:
             if myvariable[2] == "Gelb":
                 gelb = gelb + 1
+                centerlist.append(myvariable)
                 if gelb == 4:
-                    print("Gelb hat in Spalte", x+1, "gewonnen!")
+                    print("Gelb hat in Spalte", x+1, "von Links gewonnen!")
                     winnery = True
+                    # mark winning pieces
+                    for myvariable in centerlist:
+                        center = (myvariable[0], myvariable[1])
+                        #cv.circle(img, center, 1, (255, 255, 255), 2)
+                        cv.circle(img, center, radius, (255, 255, 255), 3)
                     return winnery
             else:
                 gelb = 0
+                centerlist = []
     return winnery
 
 
 # horizontal winner function
-def horwinner(test):
-    rot = 0
-    gelb = 0
+def horwinner(test, img, radius):
     winnery = False
-    for y in range(0, 6):
-        for x in range (0, 7):
+    for y in range(0, 5):
+        rot = 0
+        centerlist = []
+        for x in range(0, 6):
             spalte = test[x]
             element = spalte[y]
             if element[2] == "Rot":
                 rot = rot+1
+                centerlist.append(element)
                 if rot == 4:
-                    print("Rot hat in Zeile", x, "gewonnen")
+                    print("Rot hat in Reihe", y+1,"von oben gewonnen!")
                     winnery = True
+                    for element in centerlist:
+                        center = (element[0], element[1])
+                        #cv.circle(img, center, 1, (255, 255, 255), 2)
+                        cv.circle(img, center, radius, (255, 255, 255), 3)
                     return winnery
             else:
                 rot = 0
+                centerlist = []
 
-    for y in range(0, ):
-        for x in range (0, 7):
+    for y in range(0, 6):
+        gelb = 0
+        centerlist = []
+        for x in range(0, 6):
             spalte = test[x]
             element = spalte[y]
             if element[2] == "Gelb":
                 gelb = gelb+1
-                if rot == 4:
-                    print("Gelb hat in Zeile", x, "gewonnen")
+                centerlist.append(element)
+                if gelb == 4:
+                    print("Gelb hat in Reihe", y+1,"von oben gewonnen!")
                     winnery = True
+                    for element in centerlist:
+                        center = (element[0], element[1])
+                        #cv.circle(img, center, 1, (255, 255, 255), 2)
+                        cv.circle(img, center, radius, (255, 255, 255), 3)
                     return winnery
             else:
                 gelb = 0
+                centerlist = []
     else:
         return winnery
     return
 
 
-#who is next function
+# who is next function
 def next(start, zr, zg):
     # who is next if red started
     if start == "red":
@@ -172,17 +203,17 @@ def next(start, zr, zg):
     return
 
 
-# last free function
+# last free spot function
 def lastfree(test, img, radius):
     for x in range(0, 7):
         spalte = test[x]
-        #print("Spalte", x, ":", spalte)
+        # print("Spalte", x, ":", spalte)
         for myvariable in spalte:
 
             if myvariable[2] == "Hintergrund":
                 last = myvariable
             else:
-                print("Letzte freie Stelle in Spalte", x, ":", last)
+                # print("Letzte freie Stelle in Spalte", x, ":", last)
                 # draw circle center
                 center = (last[0], last[1])
                 cv.circle(img, center, 1, (0, 0, 0), 2)
@@ -206,3 +237,4 @@ def split(arr, size):
 # main function
 if __name__ == "__main__":
     main()
+
